77caf00e846200cbc6e363dacdb02661ec756f9f
Iiris Joutsi <iirisjoutsi@Iiriss-MacBook-Pro.local>
2024-08-13 22:45:00 +0300

    4.17 done

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const { request } = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -20,15 +21,22 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
 
   const b = request.body
+  const users = await User.find({})
+  const u = users[0]
 
   const blog = new Blog({
     title: b.title,
     author: b.author,
     url: b.url,
     likes: b.likes || 0,
+    user: u._id
   })
-
+  
   const result = await blog.save()
+
+  u.blogs = u.blogs.concat(blog._id)
+  await u.save()
+
   response.status(201).json(result)
 })
 
